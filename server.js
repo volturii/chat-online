78605579bea8b5f1944cc9679aad7342337bc2f1
volturii <121942371+volturii@users.noreply.github.com>
2024-04-01 14:17:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Serwer nasłuchuje na porcie ${PORT}`);
 });
